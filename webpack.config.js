54f320437bfbb85a0d3afeddbfe6f5efbbf625a5
Nir Asaf <nir@credex.co>
2016-07-11 09:48:23 +0300

    Webpack Production Build

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
     })
   ],
   module: {
