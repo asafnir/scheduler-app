d1c2c997a9b0b16b833abe04bc4601ca3f34f5a9
Nir Asaf <nir@credex.co>
2016-07-11 10:21:11 +0300

    Change public folder

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ if (isDeveloping) {
     res.end();
   });
 } else {
-  const indexPath = path.join(__dirname, '/../index.html')
+  const indexPath = path.join(__dirname, '../public/index.html')
   const publicPath = express.static(path.join(__dirname, '../public'))
   app.use('/public', publicPath);
   app.get('*', function response(req, res) {
